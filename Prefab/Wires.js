ddca198e6c9e559b25b8d5744e7bd86be415f8c9
Giancarlo <gtomasel@ucsc.edu>
2019-05-28 11:03:55 -0700

    fixed plug sprite bug where it would always be visable. Minigame is fully functional

---
+++ Prefab/Wires.js
@@ -0,0 +1,41 @@
+
+function Wires(game, x, y, startingHealth){
+	//sets the paramaters when created
+	Phaser.Sprite.call(this, game, x, y, 'hat');
+	game.physics.enable(this, Phaser.Physics.ARCADE);
+	this.anchor.set(0.5);
+
+	this.machineType = 'Wires';
+
+	this.Background = game.add.sprite(500,400, 'M1Background');
+	this.Background.alpha = 0;
+
+	//X value is gonna need to be tweeked
+	this.plug = game.add.sprite(625,450, 'plug');
+	this.plug.alpha = 0;
+
+	this.health = startingHealth;
+	this.healthText = game.add.text(500, 360, 'Machine health: ' + this.health);
+	this.healthText.alpha = 0;
+
+	
+
+	this.Info0 = game.add.text(600, 525, 'Hold', {fill: '#FFFFFF'});
+	this.Info0.anchor.set(0.5);
+	this.Info1 = game.add.text(625, 550, 'to fix', {fill: '#FFFFFF'});
+	this.Info1.anchor.set(0.5);
+	this.Info0.alpha = 0;
+	this.Info1.alpha = 0;
+
+	
+	
+}
+
+//Extends the Phaser.Sprite to prefab and sets the constructor name
+Wires.prototype = Object.create(Phaser.Sprite.prototype);
+
+Wires.prototype.constructor = Wires;
+
+Wires.prototype.update = function(){
+
+}
--- js/Play.js
+++ js/Play.js
@@ -12,7 +12,8 @@ Play.prototype = {
 
 
 	//Prefab instance
-	this.machine = new Generator(game, 50, 200, 50, 1);
+	//this.machine = new Generator(game, 50, 200, 50);
+	this.machine = new Wires(game, 500, 200, 50)
 	game.add.existing(this.machine);
 
 
@@ -47,6 +48,7 @@ Play.prototype = {
 	this.cursors = game.input.keyboard.createCursorKeys();
 	this.spaceBar = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 	this.keyboardW = game.input.keyboard.addKey(Phaser.Keyboard.W);
+	this.keyboardD = game.input.keyboard.addKey(Phaser.Keyboard.D);
 
 	},
 	update: function(){
@@ -62,13 +64,16 @@ Play.prototype = {
 		}
 
 		//On Overlap the machine will change the alpha of the info text (located in Generator.js)
-		var overlap = game.physics.arcade.overlap(this.player, this.machine, fixMachine, null, this);
+		//var overlap = game.physics.arcade.overlap(this.player, this.machine, fixMachine, null, this);
+		var overlap = game.physics.arcade.overlap(this.player, this.machine, fixMachineWire, null, this);
+
 		if(overlap){
 			this.machine.Info0.alpha = 1;
 			this.machine.Info1.alpha = 1;
 			this.machine.healthText.alpha = 1;
 			this.machine.Background.alpha = 1;
-			this.spacebar.alpha = 1;
+			//this.spacebar.alpha = 1;
+			this.machine.plug.alpha = 1;
 			
 		}
 		else{
@@ -77,12 +82,13 @@ Play.prototype = {
 			this.machine.healthText.alpha = 0;
 			this.machine.Background.alpha = 0;
 			this.spacebar.alpha = 0;
+			this.machine.plug.alpha = 0;
 		}
 
 		if(this.machine.health > 100){
 			this.machine.health = 100;
 		}
-		else if (this.machine.health <= 0 || this.time = 60){
+		else if (this.machine.health <= 0 || this.time > 60){
 			game.state.start('GameOver');
 		}
 
@@ -91,9 +97,42 @@ Play.prototype = {
 
 function timeEvent(){
 	this.time++;
-	this.machine.health--;
+	//this.machine.health--;
 }
 
+//Timer event for moving the plug forward and increasing health(minigame2)
+function movePlugF(machine){
+	machine.plug.x += 11.5;
+	machine.health += 5;
+	if(machine.plug.x > game.width - 72){
+		machine.plug.x = game.width - 72;
+	}
+}
+
+//Timer event for moving the plug backwards and lowering health (minigame2)
+function movePlugB(machine){
+	machine.plug.x -= 2.5;
+	machine.health -= 1;
+	if(machine.plug.x < 500){
+		machine.plug.x = 500;
+		game.time.events.remove(this.timeLoop);
+	}
+}
+
+//Overlap method called for moving the plug (minigame2)
+function fixMachineWire(player, machine){
+	if(this.keyboardD.downDuration(5)){
+		game.time.events.remove(this.timeLoop);
+		this.timeLoop = game.time.events.loop(Phaser.Timer.SECOND, movePlugF, this, machine);
+	}
+	if(this.keyboardD.upDuration(50)){
+		game.time.events.remove(this.timeLoop);
+		this.timeLoop = (game.time.events.loop(Phaser.Timer.SECOND/2, movePlugB, this, machine))
+	
+	}
+}
+
+//Overlap method called for the generator. When space bar is pressed increase health (minigame1)
 function fixMachine(player, machine){
 	if(this.spaceBar.downDuration(5)){
 		this.machine.health += 5;
