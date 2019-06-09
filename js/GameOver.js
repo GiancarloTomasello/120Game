c6157274069da9eaab857b3a4f6990f1b7752b51
Giancarlo <gtomasel@ucsc.edu>
2019-06-09 11:31:34 -0700

    Added the ability to return to the main menu from gameover screen (A little slow. Might want to add a foex loading screen

--- js/GameOver.js
+++ js/GameOver.js
@@ -2,7 +2,7 @@ var GameOver = function(game){}
 GameOver.prototype = {
 	init: function(stateName){
 		this.stateName = stateName;
-		console.log('stateName: ' + this.stateName);
+		//console.log('stateName: ' + this.stateName);
 	},
 
 	create: function(){
@@ -12,14 +12,20 @@ GameOver.prototype = {
 		this.Background.scale.setTo(0.65,0.75);
 		game.stage.backgroundColor = '#658AB2';
 
-		// var menuText = game.add.text(0, 0, 'GameOver', { fontSize: '48px', fill: '#000', boundsAlignH: 'center', boundsAlignV: 'top'});
-		// menuText.setTextBounds(0,100,800,100);
+		var menuText = game.add.text(0, 40, 'Press [space] to retry the last level', { fontSize: '24px', fill: '#000', boundsAlignV: 'top'});
+		menuText.setTextBounds(0,100,800,100);
+
+		var menuText = game.add.text(0, 90, 'Press [w] to return to the menu', { fontSize: '24px', fill: '#000', boundsAlignV: 'top'});
+		menuText.setTextBounds(0,100,800,100);
 	},
 
 	update: function(){
 		//Restart Level
 		if(game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)){
 			game.state.start(this.stateName);
+		}
+		else if(game.input.keyboard.isDown(Phaser.Keyboard.W)){
+			game.state.start('MainMenu');
 		}
 	}
 }
