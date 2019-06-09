ba610c06a9cde89a5e32112277326ed682639026
Giancarlo <gtomasel@ucsc.edu>
2019-06-09 10:45:25 -0700

    Added Title and GameOver sprites as well as resized them to properly fit the screen

--- js/GameOver.js
+++ js/GameOver.js
@@ -1,16 +1,18 @@
 var GameOver = function(game){}
 GameOver.prototype = {
-	preload: function(){
-
+	init: function(){
+		
 	},
 
 	create: function(){
 		console.log('GameOver: create');
 		//Background
+		this.Background = game.add.sprite(0,-25, 'GameOverText');
+		this.Background.scale.setTo(0.65,0.75);
 		game.stage.backgroundColor = '#658AB2';
 
-		var menuText = game.add.text(0, 0, 'GameOver', { fontSize: '48px', fill: '#000', boundsAlignH: 'center', boundsAlignV: 'top'});
-		menuText.setTextBounds(0,100,800,100);
+		// var menuText = game.add.text(0, 0, 'GameOver', { fontSize: '48px', fill: '#000', boundsAlignH: 'center', boundsAlignV: 'top'});
+		// menuText.setTextBounds(0,100,800,100);
 	},
 
 	update: function(){
--- js/MainMenu.js
+++ js/MainMenu.js
@@ -10,6 +10,8 @@ MainMenu.prototype = {
 		game.load.image('spacebar', 'assets/img/Spacebar.png');
 		game.load.image('crate', 'assets/img/Crate.png');
 		game.load.image('ground', 'assets/img/Tile.png');
+		game.load.image('TitleScreen', 'assets/img/TitleScreen.png');
+		game.load.image('GameOverText', 'assets/img/EndScreen.png');
 		game.load.atlas('player', 'assets/img/maron.png', 'assets/img/Maron.json');
 		game.load.audio('fix000', 'assets/audio/FixSoundFX.wav');
 		game.load.audio('fix001', 'assets/audio/FixSoundFX001.wav')
@@ -18,13 +20,16 @@ MainMenu.prototype = {
 	create: function(){
 		console.log('MainMenu: create');
 		//Background
+
+		this.title = game.add.sprite(0,0, 'TitleScreen');
+		this.title.scale.setTo(0.65,.75)
 		game.stage.backgroundColor = '#facade';
 
-		var menuText = game.add.text(0, 0, 'Balncing Buisness', { fontSize: '48px', fill: '#000', boundsAlignH: 'center', boundsAlignV: 'top'});
-		menuText.setTextBounds(0,100,800,100);
+		// var menuText = game.add.text(0, 0, 'Balncing Buisness', { fontSize: '48px', fill: '#000', boundsAlignH: 'center', boundsAlignV: 'top'});
+		// menuText.setTextBounds(0,100,800,100);
 
-		var menuText = game.add.text(0, 75, 'by group 29', { fontSize: '24px', fill: '#000', boundsAlignH: 'center', boundsAlignV: 'top'});
-		menuText.setTextBounds(0,100,800,100);
+		// var menuText = game.add.text(0, 75, 'by group 29', { fontSize: '24px', fill: '#000', boundsAlignH: 'center', boundsAlignV: 'top'});
+		// menuText.setTextBounds(0,100,800,100);
 
 		var menuText = game.add.text(0, 400, 'Press [space] to start', { fontSize: '24px', fill: '#000', boundsAlignH: 'center', boundsAlignV: 'top'});
 		menuText.setTextBounds(0,100,800,100);
@@ -33,7 +38,7 @@ MainMenu.prototype = {
 
 	update: function(){
 		if(game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)){
-			game.state.start('Level-4');
+			game.state.start('GameOver');
 		}
 	}
 }
